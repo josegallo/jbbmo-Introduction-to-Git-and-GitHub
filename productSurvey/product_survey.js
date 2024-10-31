dcbbbb07c57e9af997dc8315fc9c5abda6098df6
Jose Gallo <josegallo@gmail.com>
2024-10-31 16:05:19 -0400

    add user experiencie feedback

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -9,6 +9,8 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const feedbackUserExperince = document.getElementById('userExperience').value;
+    
 
     alert('Thank you for your valuable feedback');
 
@@ -19,6 +21,8 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperiencieFeedback').innerHTML = feedbackUserExperince;
+    
 
     document.getElementById('userInfo').style.display = 'block';
 }
